9c960ba3275c8698752342da104b399c9651b7e1
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-10-09 12:56:54 +0300

    loaded project files

---
+++ src/utils/predator.py
@@ -0,0 +1,8 @@
+class Predator:
+    def __init__(self, a, b, c, d, name, target_data):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.name = name
+        self.target_data = target_data
